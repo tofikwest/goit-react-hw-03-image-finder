1ac85658b2d348f65769d2abbe631b274f3ebc5b
Tofik Hasanov <annexcies@gmail.com>
2021-07-28 19:40:22 +0400

    clear trash

--- Новая папка/src/components/Container/Container.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-importStylefrom './Container.module.css';
-
-const Container = ({ children }) => (
-  <div className={Style.container}>{children}</div>
-);
-
-export default Container;
